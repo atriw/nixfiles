b644b009611606119fe39d631833f4a61201d39c
atriw <875241499@qq.com>
2023-06-28 04:22:30 +0800

    GHC

--- hmModules/lang/haskell.nix
+++ hmModules/lang/haskell.nix
@@ -1,10 +1,26 @@
-{ config, lib, pkgs, ... }:
-with lib;
-let cfg = config.modules.lang.haskell;
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.modules.lang.haskell;
 in {
-  options = { modules.lang.haskell = { enable = mkEnableOption "Haskell"; }; };
+  options = {modules.lang.haskell = {enable = mkEnableOption "Haskell";};};
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [ ghc cabal-install haskell-language-server ];
+    home.packages = with pkgs; [
+      (ghc.withPackages (pkgs:
+        with pkgs; [
+          vector
+          unordered-containers
+          microlens-platform
+          foldl
+          random
+        ]))
+      cabal-install
+      haskell-language-server
+    ];
   };
 }
